1c4f34ee54ddeec3c0531aad97cd839ce80a4d54
Jonas Drotleff <jonas@drtlf.de>
2021-05-11 12:26:21 +0200

    Add test for data_uri

--- tests/render_test.py
+++ tests/render_test.py
@@ -5,6 +5,12 @@ from django.template import Context, Template
 class FeatherTemplateTest(TestCase):
     def test_render(self):
         context = Context({})
-        template_to_render = Template("{% load icon %}" '{% icon "coffee" %}')
+        template_to_render = Template("{% load icon %}{% icon 'coffee' %}")
         rendered_template = template_to_render.render(context)
         self.assertIn("<svg", rendered_template)
+
+    def test_base64(self):
+        context = Context({})
+        template_to_render = Template("{% load icon %}{% icon 'coffee' data_uri=True %}")
+        rendered_template = template_to_render.render(context)
+        self.assertTrue(rendered_template.startswith("data:image/svg+xml;base64"))
